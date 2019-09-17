2dba96273d38b8720bb2b34da9fc0a163d508ca0
Sanal Bhatia <sanal.bhatia@warwick.ac.uk>
2019-09-17 19:11:05 +0100

    Initial Commit

---
+++ TaskBar-Crypto/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TaskBar-Crypto
+//
+//  Created by Sanal Bhatia on 17/09/2019.
+//  Copyright © 2019 Sanal Bhatia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TaskBar-Crypto/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TaskBar-Crypto
+//
+//  Created by Sanal Bhatia on 17/09/2019.
+//  Copyright © 2019 Sanal Bhatia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
